e04a5a496339f12288bc0acc2a30aee1f5b63952
Quang Oliu <trantran30102002@gmail.com>
2022-10-19 21:39:10 +0700

    init project

---
+++ src/main/GamePanel.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	public GamePanel() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.fillRect(100, 50, 100, 50);
+	}
+}
---
+++ src/main/GameWindow.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+	private JFrame Jframe;
+
+	public GameWindow(GamePanel gamePanel) {
+		Jframe = new JFrame();
+
+		Jframe.setSize(400, 400);
+		Jframe.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
+		Jframe.add(gamePanel);
+		Jframe.setVisible(true);
+	}
+}
