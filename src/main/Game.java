889879b380a3ef362b49a74b8c3d93ae26953d1f
Quang Oliu <trantran30102002@gmail.com>
2022-12-02 15:22:19 +0700

    Add hightspeed

--- src/main/Game.java
+++ src/main/Game.java
@@ -112,7 +112,7 @@ public class Game implements Runnable {
 
 			if (System.currentTimeMillis() - lastCheck >= 1000) {
 				lastCheck = System.currentTimeMillis();
-				// System.out.println("FPS: " + frames + " | UPS: " + updates);
+				System.out.println("FPS: " + frames + " | UPS: " + updates);
 				System.out.println("HIGHT SPEED: "+playing.getPlayer().getHightSpeed());
 				frames = 0;
 				updates = 0;
